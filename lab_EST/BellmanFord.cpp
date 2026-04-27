c8e3f1d62030847af70064ee991855ad75a095f8
ShreyanshGupta-02 <shreyanshgupta191@gmail.com>
2026-04-27 16:05:26 +0530

    Create BellmanFord.cpp

---
+++ lab_EST/BellmanFord.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class solution{
+public:
+    vector<int> bellman_ford(int V, vector<vector<int>> edges, int src){
+        vector<int> dist(V,1e8);
+        dist[src]=0;
+        for(int i=0;i<V-1;i++){
+            for(auto edge:edges){
+                int u=edge[0];
+                int v=edge[1];
+                int w=edge[2];
+                if(dist[u]!=1e8 && dist[u]+w<dist[v]){
+                    dist[v]=dist[u]+w;
+                }
+            }
+        }
+        for(auto edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            int w=edge[2];
+            if(dist[u]!=1e8 && dist[u]+w<dist[v]){
+                return {-1};
+            }
+        }
+        return dist;
+    }
+};
+
